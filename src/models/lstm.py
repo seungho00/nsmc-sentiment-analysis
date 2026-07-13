739145d5d2dd3b785d90bb3b738f92665abef1c2
seungho00 <133303280+seungho00@users.noreply.github.com>
2026-07-13 22:19:30 +0900

    lstm 모델 구현

---
+++ src/models/lstm.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+class SentimentLSTM(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_size):
+        super().__init__()
+
+        self.embedding = nn.Embedding(
+            num_embeddings=vocab_size,
+            embedding_dim=embedding_dim,
+            padding_idx=0
+        )
+
+        self.double_lstm = nn.LSTM(
+            input_size=embedding_dim,
+            hidden_size=hidden_size,
+            num_layers=2,
+            batch_first=True
+        )
+
+        self.fc = nn.Linear(
+            in_features=hidden_size,
+            out_features=1
+        )
+
+    def forward(self, x):
+        x = self.embedding(x)
+        
+        output, (h_n, c_n) = self.double_lstm(x)
+
+        x = self.fc(h_n[-1]).squeeze(1)
+
+        return x
